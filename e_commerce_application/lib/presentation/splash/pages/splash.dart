806abd4f634f4c7357eadf81efd67a6f513e734e
arkise-demise <arkisewdemisew14@gmail.com>
2025-04-29 17:19:11 +0300

    lib folder added

---
+++ e_commerce_application/lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,13 @@
+import 'package:e_commerce_application/core/configs/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+    );
+  }
+}
